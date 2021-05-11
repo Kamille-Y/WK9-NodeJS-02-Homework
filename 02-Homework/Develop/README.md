028488af7c0813890756c30f6d5c74143489bec5
Kamille <tramia25@gmail.com>
2021-05-10 21:24:17 -0400

    completed and functioning readmegenerator with an example

---
+++ 02-Homework/Develop/README.md
@@ -0,0 +1,47 @@
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  # Project Title
+  Readme Generator
+  ##  Description
+   a professional readme generator.
+
+  ## Table of Contents
+
+* [Description](#description)
+* [Usage](#usage)
+* [License](#license)
+* [Contribution](#contribution)
+* [Usage](#usage)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+To install dependencies, run the below command :
+  The repository has all of the needed libraries and dependencies.
+
+  ## Usage
+   This readme generator will overwrite previously created readme's so save in a new folder each use.
+
+   ## License
+
+   
+This repository is covered under the license 'MIT License' 
+Refer https://opensource.org/licenses/MIT for more detailed info 
+  
+
+## Contribution
+   undefined
+## Tests
+   undefined
+
+
+## Questions?
+
+I can be reached @
+Email: [tramia25@gmail.com](mailto:undefined)
+GitHub: [Kamille-Y](https:// github.com/undefined)
+ 
+   
+
--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -2,18 +2,18 @@
 var fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-
+const path = require("path");
 // TODO: Create an array of questions for user input for dot notation it is better to use single brackets could be used for phrases (name) declares varriable name
-inquirer
-  .prompt([
+
+  const questions=[
     {
       type: "input",
-      name:"name",
+      name: "title",
       message: "What Is Your Project Name?"
     },
     {
       type: "input",
-      name: "Description",
+      name: "description",
       message: "Brief Description Of The Application"
     },
     {
@@ -21,58 +21,61 @@ inquirer
       name: "usage",
       message: "Please Enter Usage Info"
     },
-    {
+     {
       type: "input",
       name: "installation",
       message: "Enter Installation Instructions Here"
     },
     {
       type: "input",
-      name:"contributions",
+      name: "contributions",
       message: "Any contribution Guidelines?"
     },
     {
       type: "input",
-      name: "GitHub",
-      message: "git"
+      name: "github",
+      message: "GitHub link"
     },
     {
       type: "input",
-      name: "Email",
+      name: "email",
       message: "e-mail"
     },
     {
       type: "input",
-      name: "Linkedin",
+      name: "linkedin",
       message: "Enter Linkedin Profile Address."
     },
     {
       type: "input",
-      name: "Test",
+      name: "test",
       message: "Enter Test Instructions Here."
     },
     {
       type: "list",
-      name: "License",
+      name: "license",
       message: "What Liscence Did You Use",
       choices: [
-        "GNU GPLv3, Mozilla Public License 2.0, Apache License 2.0, MIT License, Boost Software License 1.0, The Unlicense, BSD 3-Clause"
+        "GNU GPLv3",
+        "Mozilla Public License 2.0", 
+        "Apache License 2.0", 
+        "MIT License", 
+        "Boost Software License 1.0",
+         "The Unlicense", 
+         "BSD 3-Clause",
       ]
     }
-  ])
+  ];
+  function writeToFile(fileName, data){
+    return fs.writeFileSync(path.join(process.cwd(),fileName), data);
+   }
   //function to write README file then passes in to the generate markdown
-  .then((answers) => {
-    const READMEcontent = generateMarkdown(answers);
-    writeToFile(".output/README.md", READMEcontent);
-  });
-
-function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
 // TODO: Create a function to initialize app
 function init() {
-  // console.log( "Hello Please answner the following questions");
-  promptUser(questions);
+  inquirer.prompt(questions).then((inquirerResponses)=>{
+    console.log("generateREADME");
+    writeToFile("README.md", generateMarkdown({...inquirerResponses}));
+  });
 }
 // Function call to initialize app
 init();
